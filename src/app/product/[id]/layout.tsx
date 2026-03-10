bdc999033cba465414076665b0768dfea1aa1f0d
EDUARDO SOARES TONON <D26385@academico.domhelder.edu.br>
2026-03-10 20:13:59 -0300

    update

--- src/app/product/[id]/layout.tsx
+++
@@ -1,9 +0,0 @@
- import { PRODUCTS } from '@/lib/data'
- 
- export default function ProductLayout({ children }: { children: React.ReactNode }) {
-   return children
- }
- 
- export function generateStaticParams() {
-   return PRODUCTS.map(p => ({ id: p.id }))
- }
